5b7113cfa2bca610f8777854e8fe80d55d2e1505
lucasnfd <lucasdanielnf9@gmail.com>
2024-10-16 13:08:28 -0300

    CursoPython3

--- aula1.py
+++ aula1.py
@@ -1,1 +1,12 @@
-print("Olá Mundo!")
+"""
+DocStrings não é um comentario,
+e sim usado para escrever o que 
+quiser.
+
+Ex: anotações
+"""
+'''Usar para escrever suas notas '''
+# Permite escrever um cmentário
+
+print('Agora sou dev.')
+#
