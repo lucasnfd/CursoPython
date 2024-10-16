2d026ba478f26404585c4272e4d2ebf3f5ec5d6f
lucasnfd <lucasdanielnf9@gmail.com>
2024-10-16 14:20:26 -0300

    CursoPython

---
+++ aula4.py
@@ -0,0 +1,15 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: inicia variáveis com letras minúsculas, pode usar números e underline_.
+# O sinal de = e o operador de atribuição. Ele é usado para atribuir um valor a um nome(variável).
+# Uso: nome_variavel = expressão
+# soma_dois_mais_dois = 2 + 2
+# nome_completo = 'Lucas Daniel N Freire'
+# int_um = int('1')
+# print(int_um, type(int_um))
+# print(nome_completo) # Lucas Daniel N Freire.
+
+nome = 'Lucas'
+idade = 14
+maior_de_idade = idade >= 18
+print('Nome:', nome, 'Idade:', idade)
+print('É maior?', maior_de_idade)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+soma_dois_mais_dois = 2 + 2
+# nome_completo = 'Lucas Daniel N Freire'
+# int_um = int('1')
+# print(int_um, type(int_um))
+# print(nome_completo) # Lucas Daniel N Freire.
