abeb6aac46e702e19b1f569b03e158337fb47875
lucasnfd <lucasdanielnf9@gmail.com>
2024-10-25 11:16:00 -0300

    CursoPython3

---
+++ aula11.py
@@ -0,0 +1,33 @@
+# Operações condicionais ao blocos de codigos if/elif/else(condicionais)
+
+# if / elif ****** / else
+# se / se não se   / se não
+
+# if / elif      / else
+# se / se não se / se não
+
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = True
+
+if condicao1:
+    print('Código para condição 1')
+    print('Código para condição 1')
+elif condicao2:
+    print('Código para condição 2')
+elif condicao3:
+    print('Código para condição 3')
+elif condicao4:
+    print('Código para condição 4')
+else:
+    print('Nenhuma condição foi satisfeita.')
+
+if 10 == 10:
+    print('Outro if')
+
+print('Fora do if')
+
+
+
+# O interpretador só executará o bloco condicional ao chegar nele
