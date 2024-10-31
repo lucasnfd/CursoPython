16c3b870de81545d55cb2e35d752a04952a67040
lucasnfd <lucasdanielnf9@gmail.com>
2024-10-31 14:43:36 -0300

    CursoPython

---
+++ aula12.py
@@ -0,0 +1,18 @@
+"""
+Operadores de comparação (relacionais)
+OP            Significado         Exemplo (True)
+>              maior               2 > 1 
+>=             maior ou igual      2 >= 2
+<              menor               1 < 2
+<=             menor ou igual      2 <= 2
+==             igual               'a' == 'a' 
+!=             diferente           'a' != 'a'
+
+"""
+maior = 2 > 1
+maior_ou_igual = 2 >= 2
+menor = 1 < 2 
+manor_ou_igual = 2 <= 2
+igual = 'a' == 'a'
+diferente = 'a' != 'a'
+print(maior_ou_igual)
