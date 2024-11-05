dd82dc96d6286ec69266da7a557d3f76528eae47
lucasnfd <lucasdanielnf9@gmail.com>
2024-11-05 14:37:10 -0300

    CursoPython3

--- aula13.py
+++ aula13.py
@@ -4,10 +4,6 @@ segundo_valor = input('Digite outro valor:')
 
 
 if primeiro_valor > segundo_valor:
-    print(
-        f'{primeiro_valor=} é maior'
-        f'do que{segundo_valor=}')
+    print(f'{primeiro_valor=} é maior ou igual ao {segundo_valor=}')
 else:
-    print(
-        f'{segundo_valor=} é maior'
-        f'do que {primeiro_valor=}')
+    print(f'{segundo_valor=} é maior do que {primeiro_valor=}')
